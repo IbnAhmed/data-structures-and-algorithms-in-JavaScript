977637f85472e09fe107179d1ff376be8578a40b
Muntaha Ibn Ahmed <ahamedmuntaha@gmail.com>
2021-07-03 12:42:17 +0600

    Added new implementation of stack using queue

--- Stack/other-implementations.js
+++ Stack/other-implementations.js
@@ -26,6 +26,58 @@ class StackWithLinkedList{
 	}
 }
 
+// Stack with two queues
+class StackWithTwoQueue{
+	queueStack = new Queue();
+	secondQueue = new Queue();
+
+	push(item){
+		this.queueStack.enQueue(item);
+		return this.queueStack.count;
+	}
+	pop(){
+		if(!this.queueStack.isEmpty()){
+
+			while(this.queueStack.count > 1){
+				this.secondQueue.enQueue(this.queueStack.deQueue());
+			}
+
+			let item = this.queueStack.deQueue();
+			
+			while(!this.secondQueue.isEmpty()){
+				this.queueStack.enQueue(this.secondQueue.deQueue());
+			}			
+
+			return item;
+		}
+		return undefined;
+	}
+	peek(){
+		if(!this.queueStack.isEmpty()){
+
+			let item = undefined;
+			while(!this.queueStack.isEmpty()){
+				item = this.queueStack.deQueue();
+				this.secondQueue.enQueue(item);
+			}
+
+			
+			while(!this.secondQueue.isEmpty()){
+				this.queueStack.enQueue(this.secondQueue.deQueue());
+			}			
+
+			return item;
+		}
+		return undefined;
+	}
+	isEmpty(){
+		return this.queueStack.count == 0;
+	}
+	print(){
+		this.queueStack.print()
+	}
+}
+
 // let newLLStack = new StackWithLinkedList();
 // if(newLLStack.isEmpty()){
 // 	newLLStack.push(20);
@@ -34,3 +86,14 @@ class StackWithLinkedList{
 
 // console.log('[Stack With LinkedList] Top value', newLLStack.peek());
 // console.log('[Stack With LinkedList] Top value removed', newLLStack.pop());
+
+let newQStack = new StackWithTwoQueue();
+if(newQStack.isEmpty()){
+	newQStack.push(20);
+	newQStack.push(31);
+}
+
+newQStack.print();
+console.log('[Stack With Queue] Top value', newQStack.peek());
+console.log('[Stack With Queue] Top value removed', newQStack.pop());
+newQStack.print();
